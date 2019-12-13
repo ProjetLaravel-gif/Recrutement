65446decc8c2f21881084fe4543b77c1bac9c359
yasmine <yasminefandi10@gmail.com>
2019-12-13 20:07:45 +0100

    modification du menu->lien ajouter cv

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -54,29 +54,25 @@
                <div class="account-card">
                  <div class="header-top-account-info">
                    <a href="#" class="account-thumb">
-                   
+                     <img src="images/account/thumb-1.jpg" class="img-fluid" alt="">
+                   </a>
+                   <div class="account-body">
+                     <h5><a href="#">Robert Chavez</a></h5>
+                     <span class="mail">chavez@domain.com</span>
+                   </div>
                  </div>
                  <ul class="account-item-list">
-
-
-                   <li class="nav-item dropdown">
-                    
-                      <li><a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('lol') }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="position: relative; padding-right: 50px; ">
-                    <img src="images/avatars/{{ Auth::guard('candidat')->user()->avatar }}" style="width: 80px;  height: 80px; position:absolute; top: 10px; right: 10px; border-radius: 50%; " >
-                                   <b>{{ Auth::guard('candidat')->user()->nom }} {{ Auth::guard('candidat')->user()->prenom }}</b>  <span class=""></span>
-                                </a></li> 
-                            
                    <li><a href="#"><span class="ti-user"></span>Account</a></li>
                    <li><a href="#"><span class="ti-settings"></span>Settings</a></li>
                    <li> <a  href="{{ route('profil') }}"> mon profil </a> <br>
-                   <li> <a  href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        logout
-                                    </a></li>
-                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form><span class="ti-power-off"></span>Log Out</a></li>
+                 <li> <a  href="{{ route('logout') }}"
+                                                        onclick="event.preventDefault();
+                                                                      document.getElementById('logout-form').submit();">
+                                                         logout
+                                                     </a></li>
+                                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                         @csrf
+                                                     </form><span class="ti-power-off"></span>Log Out</a></li>
                  </ul>
                </div>
              </div>
@@ -107,7 +103,7 @@
                  <ul  class="dropdown-menu">
                    <li class="menu-item"><a  href="candidate.html">Candidate Listing</a></li>
                    <li class="menu-item"><a  href="candidate-details.html">Candidate Details</a></li>
-                   <li class="menu-item"><a  href="add-resume.html">Add Resume</a></li>
+                   <li class="menu-item"><a  href="{{ url('cvs/create/$cvs->candidat_id')}}">Add Resume</a></li>
                  </ul>
                </li>
                <li class="menu-item dropdown">
